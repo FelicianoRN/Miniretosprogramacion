ff951fac48377aeff52b59268d67a63d8a8901e2
felicianorn <feliciano.ramirez.navarro.al@iespoligonosur.org>
2024-12-10 23:46:35 +0100

    cambios en almacen

--- Almacen/Descifrar_codigo.py
+++ Almacen/Descifrar_codigo.py
@@ -1,15 +1,15 @@
 import random
 
 def generar_codigo_secreto():
-    letras = ['A', 'B', 'C']
-    numeros = ['1', '2', '3']
+    letras = ['A', 'B', 'C','D', 'E']
+    numeros = ['1', '2', '3', '4', '5']
     caracteres = letras + numeros
-    codigo_secreto = random.sample(caracteres, 4)
+    codigo_secreto = random.sample(caracteres, 5)
     return ''.join(codigo_secreto)
 
 def obtener_pistas(codigo_secreto, intento):
     pistas = []
-    for i in range(4):
+    for i in range(5):
         if intento[i] == codigo_secreto[i]:
             pistas.append('Correcto')
         elif intento[i] in codigo_secreto:
@@ -19,17 +19,17 @@ def obtener_pistas(codigo_secreto, intento):
     return pistas
 
 def validar_intento(intento):
-    if len(intento) != 4:
+    if len(intento) != 5:
         return False
     for char in intento:
-        if char not in 'ABC123':
+        if char not in 'ABCDE12345':
             return False
     return True
 
 def main():
     codigo_secreto = generar_codigo_secreto()
     print("¡Bienvenido al juego de descifrado de códigos de Amazon!")
-    print("Debes adivinar el código secreto de 4 caracteres (A-C, 1-3).")
+    print("Debes adivinar el código secreto de 5 caracteres (A-E, 1-5).")
     print("Tienes 10 intentos. ¡Buena suerte!")
 
     intentos_restantes = 10
@@ -38,7 +38,7 @@ def main():
         intento = input("Introduce tu intento: ").upper()
         
         if not validar_intento(intento):
-            print("Intento inválido. Asegúrate de que el código tenga 4 caracteres y solo contenga A, B, C, 1, 2, 3.")
+            print("Intento inválido. Asegúrate de que el código tenga 5 caracteres y solo contenga A, B, C, D, E 1, 2, 3, 4, 5.")
             continue
 
         pistas = obtener_pistas(codigo_secreto, intento)
